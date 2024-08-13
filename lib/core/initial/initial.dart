d112b5fc61a9c0249bf25e1e7ca15f141fe94e66
Mert Erim <mert.erim1@gmail.com>
2024-08-13 11:08:52 +0300

    refactor: Remove unnecessary Hive initialization code

--- lib/core/initial/initial.dart
+++ lib/core/initial/initial.dart
@@ -1,10 +1,9 @@
-import 'package:hive_flutter/hive_flutter.dart';
+
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:tic_tac_toe_with_supabase/core/config/config.dart';
 
 class InitialRequired {
   static Future<void> initRequireds() async {
-    await Hive.initFlutter('orman');
     await Supabase.initialize(
       url: Env.supabaseUrl,
       anonKey: Env.key,
